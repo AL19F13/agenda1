3e99bf268e9995b934ff57aeb8b255af8b822120
AL19F13 <arturoizucar3@gmail.com>
2015-08-12 11:39:14 -0500

    uno
    agenda

--- js/acciones.js
+++ js/acciones.js
@@ -1,17 +1,19 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
+document.addEventListener("deviceready",function(){	
+alert("dispositivo listo")
 var basedatos=window.sqlitePlugin.opendatabase({name: "agenda.db",createFromLocation:1});
-
+alert("despues de la base")
 	cargarnombrecontacto();
 	function cargarnombrecontacto ()
 	{
+		alert("despues de la carga")
 		basedatos.transaction(function(ejecutar){
 			var sql="sELECT nombre_contacto FROM contactos";
 			ejecutar.excecuteSql(sql,undifined,function(ejecutar,resultado){
 				var datoscontacto=resultado.rows.item(o);
-				var actual = datoscontacto.nombre_contacto;
-				$(alert).text(actual);
+				var nombre = datoscontacto.nombre_contacto;
+				alert(nombre);
 			});
 		});
 	}
